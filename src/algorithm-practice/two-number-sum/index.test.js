4a7d5c567abf4aa6135d35d8e4d188011e5f0504
Fasasi Toheeb <tohebayomide11@gmail.com>
2022-06-28 15:07:06 +0100

    feat: begin algorithm pratice

---
+++ src/algorithm-practice/two-number-sum/index.test.js
@@ -0,0 +1,10 @@
+import {it, expect, describe} from 'vitest';
+
+import {sumOfTwoNumber} from './'
+
+describe('sum()', () => {
+  it('should return the addition of the two number given', () => {
+
+    expect(6).toBe(6)
+  })
+})
